2cc5573f1319e52f7161b09f6c579d8642021d0e
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-12-05 18:21:41 +0530

    ADDED: EmployeeManagement.Models

---
+++ EmployeeManagement.Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeManagementSystem;
+
+public class Employee
+{
+	public int Employeeid { get; set; }
+	public string FirstName { get; set; } = string.Empty;
+	public string LastName { get; set; } = string.Empty;
+	public string Email { get; set; }
+	public DateTime DateOfBirth { get; set; }
+	public Gender Gender { get; set; }
+	public int DepartmentId { get; set; }
+	public string PhotoPath { get; set; }
+}
+
+public enum Gender
+{
+	Male,
+	Female,
+	Other
+}
+
+public class Department
+{
+	public int DepartmentId { get; set; }
+	public string DepartmentName { get; set; }
+}
